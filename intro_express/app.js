96226057c7badc7ddd37bf199365c53c8437fa14
Vikash Kumar Sharma <vikashkumar97983282@gmail.com>
2026-02-23 23:59:51 +0530

    change in file.

--- intro_express/app.js
+++ intro_express/app.js
@@ -1,11 +1,22 @@
-const http = require('http');
-const requesthandler = require('./handler');
+const express = require('express');
+
+const app = express();
+
+// adding middleware
+app.use((req,res,next)=>{
+    console.log("this is first middleware ",req.url, req.method);
+    next();
+});
+
+app.use((req,res,next)=>{
+    console.log("this is second middleware", req.url, req.method);
+    res.send("<p>welcome to home page. this is created by vikash kumar sharma</P>")
+});
 
 
-const server = http.createServer(requesthandler);
+
 
 const PORT = 3005;
-
-server.listen(PORT,()=>{
+app.listen(PORT,()=>{
     console.log(`sever running on http://localhost:${PORT}`);
 });
