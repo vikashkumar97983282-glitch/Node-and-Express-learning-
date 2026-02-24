e4f294cfc2c83b650f630045d6f4f62cf0c3cef6
Vikash Kumar Sharma <vikashkumar97983282@gmail.com>
2026-02-25 00:56:59 +0530

    add unknown path handle page .

--- routing_express/app.js
+++ routing_express/app.js
@@ -16,6 +16,12 @@ app.use(hostRouter);
 app.use(aboutRouter);
 
 
+//handling page 
+app.use((req,res,next)=>{
+    res.status(404).send("<h1>404 page not found!</h1>")
+})
+
+
 
 const PORT = 3001;
 app.listen(PORT,()=>{
