48f74679e5643450437cf7fda6c007ff5c3530c3
Vikash Kumar Sharma <vikashkumar97983282@gmail.com>
2026-02-26 00:22:02 +0530

    seperate and file handling error page.

--- express_deepDrive_path/app.js
+++ express_deepDrive_path/app.js
@@ -1,10 +1,14 @@
+// core modules
+const path = require('path');
+
+
 // External modules
 const express = require('express');
 
 // local modules
 const userRouter = require('./routing/userRouter');
 const hostRouter = require('./routing/hostRouter');
-const aboutRouter = require('./routing/aboutRouter')
+const aboutRouter = require('./routing/aboutRouter');
 
 const app = express();
 
@@ -18,8 +22,8 @@ app.use(aboutRouter);
 
 //handling page 
 app.use((req,res,next)=>{
-    res.status(404).send("<h1>404 page not found!</h1>")
-})
+    res.status(404).sendFile(path.join(__dirname, './', 'views', 'error.html'))
+});
 
 
 
