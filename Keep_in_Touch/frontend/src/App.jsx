6a5be01ae4393572f3c129076b5990aff3e8ee9c
Vikash Kumar Sharma <vikashkumar97983282@gmail.com>
2026-03-27 21:31:15 +0530

    add api for credentials.

--- Keep_in_Touch/frontend/src/App.jsx
+++ Keep_in_Touch/frontend/src/App.jsx
@@ -1,119 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useState } from 'react';
 import './App.css'
+import axios from 'axios'
+import { useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const [data,setData] = useState([]);
+
+  useEffect(()=>{
+    const fetch = async ()=>{
+
+    try{
+      const res = await axios.get("http://localhost:3000/show");
+      console.log(res.data);res.data
+      setData(res.data)
+    } catch(err){
+      console.log(err)
+    }
+  }
+  fetch();
+  
+  },[])
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
+      <div className="bg-red-600">WELCOME TO KEEP_IN_TOUCH</div>
       <section id="spacer"></section>
+      <button onClick={fetch}>click me</button>
+      <div className='flex flex-wrap gap-2 ml-2'>{data.map((user,idx)=>{
+        return <div key={idx} className='w-70 h-40 bg-amber-400 mt-5  gap-2 flex flex-col items-center justify-center text-2xl'>
+          <h1>{user.name}</h1>
+          <h1>{user.username}</h1>
+          <h1>{user.email}</h1>
+        </div>
+       
+      })}</div>
+      <a href='./form'>go to form</a>
     </>
   )
 }
